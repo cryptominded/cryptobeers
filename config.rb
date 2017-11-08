ddef99fad563c31ab2ad75c6aa87a20405cdb107
Dylan Damsma <dylandamsma@gmail.com>
2017-11-08 23:32:48 +0100

    scss framework & ruby versioning

--- config.rb
+++ config.rb
@@ -13,6 +13,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# Live reload
+activate :livereload
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
